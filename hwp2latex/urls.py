2c09056e87fef54b18a9390acc4e30fcf963ed5f
Libattery <Libattery@gmail.com>
2017-12-27 23:47:27 +0900

    converter working

--- hwp2latex/urls.py
+++ hwp2latex/urls.py
@@ -13,12 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'', include('blog.urls')),
-    url(r'', include('hwp2latex.urls')),
+    url(r'^get_name$', views.get_name, name='get_name'),
+    url(r'^converter/$', views.converter, name='converter'),
+
 ]
--- hwp2latex/views.py
+++ hwp2latex/views.py
@@ -1,3 +1,56 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+import hml_equation_parser as hp
+
+from .forms import NameForm
+from .forms import eqInputForm
+
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            hwpeq = form.cleaned_data['your_name'] #           hwpeq = your_name
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+#            return HttpResponseRedirect('')
+#            return HttpResponseRedirect('')
+            form = NameForm()
+            return render(request, 'hwp2latex/name.html', {'hwpeq': hwpeq, 'form': form})
+#            return redirect('get_name',{'form': form})
+
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'hwp2latex/name.html', {'form': form})
+
+def converter(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = eqInputForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            hwpeq = form.cleaned_data['input_eq'] #           hwpeq = your_name
+            # process the data in form.cleaned_data as required
+            latexeq = hp.eq2latex(hwpeq)
+            # redirect to a new URL:
+#            return HttpResponseRedirect('')
+#            return HttpResponseRedirect('')
+            form = eqInputForm()
+            return render(request, 'hwp2latex/converter.html', {'hwpeq': hwpeq,'latexeq': latexeq,'form': form})
+#            return redirect('converter',{'form': form})
+
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = eqInputForm()
+
+    return render(request, 'hwp2latex/converter.html', {'form': form})
